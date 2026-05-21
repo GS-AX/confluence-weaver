535996a50edc8df6b4482af3d7e30b4899e4a7a0
Hyukjin Kwon <hyukjin78@gmail.com>
2026-05-21 22:40:38 +0900

    fix: replace builtin-modules with Node.js builtinModules

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from 'esbuild';
 import process from 'process';
-import builtins from 'builtin-modules';
+import { builtinModules } from 'module';
 
 const prod = process.argv[2] === 'production';
 
@@ -27,7 +27,7 @@ Source: https://github.com/hyukjin78/confluence-weaver
     '@lezer/common',
     '@lezer/highlight',
     '@lezer/lr',
-    ...builtins,
+    ...builtinModules,
   ],
   format: 'cjs',
   target: 'es2018',
